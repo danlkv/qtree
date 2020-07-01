72c88adacc55e9f1a535e6448f9171539414f249
Filip Mazurek <22063152+filipmazurek@users.noreply.github.com>
2020-07-01 11:11:03 -0400

    Creates full_matrix operators

--- qtree/__init__.py
+++ qtree/__init__.py
@@ -1,5 +1,11 @@
+FULL_MATRIX_MODE = True
+
 from . import graph_model
-from . import operators
 from . import optimizer
 from . import utils
 from . import np_framework
+
+if FULL_MATRIX_MODE:
+    from . import operators_full_matrix
+else:
+    from . import operators
