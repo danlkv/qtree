c88e79f7865628b5140d5ed1ac3f2cc49232ea1a
Danil Lykov <lkv97dn@gmail.com>
2021-08-02 10:14:36 -0500

    add better resolver for tamaki

--- qtree/system_defs.py
+++ qtree/system_defs.py
@@ -21,12 +21,15 @@ if not QUICKBB_COMMAND:
         QUICKBB_COMMAND = quickbb_path
 if not QUICKBB_COMMAND:
     log.warn('QuickBB solver is unavailable')
-        
+
 # Check for Tamaki solver
 try:
-    tw_loc = shutil.which('tw-exact')
-    if tw_loc:
-        TAMAKI_SOLVER_PATH = os.path.dirname(tw_loc)
+    exact_loc = shutil.which('tw-exact')
+    heuristic_loc = shutil.which('tw-heuristic')
+    if heuristic_loc:
+        TAMAKI_SOLVER_PATH = os.path.dirname(heuristic_loc)
+    elif exact_loc:
+        TAMAKI_SOLVER_PATH = os.path.dirname(exact_loc)
     else:
         TAMAKI_SOLVER_PATH = None
     if TAMAKI_SOLVER_PATH is None:
@@ -40,7 +43,7 @@ try:
         else:
             raise Exception(f'No path {tamaki_solver_path}')
 except Exception as e:
-    log.warn(f'Tamaki solver is unavailable: {e}')
+    log.warn(f'Tamaki solver is unavailable: {e}. Either install tamaki in {THIRDPARTY_PATH}/tamaki_treewidth or add `tw-heuristic` to your $PATH')
 
 MAXIMAL_MEMORY = 1e22   # 100000000 64bit complex numbers
 NP_ARRAY_TYPE = np.complex64
